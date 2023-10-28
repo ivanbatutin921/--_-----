5a97bf0175ac6974fdc32d715c41c721e57fc152
dopamine <ivanbatutin921@mail.ru>
2023-10-28 17:04:14 +0500

    Авторизация и вход

---
+++ Mysql/DataBase.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mysql
+{
+    internal class DataBase
+    {
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=root;database=worker");
+
+        public void openConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+                connection.Open();
+            
+            //если мы не подключены, то мы подключимся к базе данных
+        }
+
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+                connection.Close();
+            //если мы подключены, то мы отключимся от базы данных
+        }
+
+        public MySqlConnection getConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ Mysql/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mysql
+{
+    internal class Program:Register
+    {
+        static void Main(string[] args)
+        {
+            Register r = new Register();
+            r.Register_User();
+
+            //Login login = new Login();
+            //login.Login_User();
+        }
+    }
+}
